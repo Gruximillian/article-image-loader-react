5e9a7e3d78ae9ffc09039251f5b8473b8e6a68a2
Vojislav Grujić <vojislav.grujic@gmail.com>
2019-10-04 19:06:22 +0200

    Add basic image loader code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+const imageNames = ['galaxies.png', 'bridge.jpg', 'stars.jpg', 'sombrero.jpg'];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [numberOfTimesLoaded, setNumberOfTimesLoaded] = useState(0);
+    const [imagePath, setImagePath] = useState(undefined);
+
+    function loadImage() {
+        setNumberOfTimesLoaded(numberOfTimesLoaded + 1);
+        const imageName = imageNames[numberOfTimesLoaded % imageNames.length];
+        setImagePath(`img/${imageName}`);
+    }
+
+    return (
+        <div>
+            <div className="container button-container">
+                <button
+                    className="load-button"
+                    onClick={loadImage}
+                >
+                    Load Image
+                </button>
+            </div>
+
+            <div className="container image-container-with-loader">
+                {
+                    imagePath &&
+                    <img src={imagePath} alt="image placeholder"/>
+                }
+            </div>
+        </div>
+    );
 }
 
 export default App;
